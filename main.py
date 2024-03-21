fdc6e04d6de0ca1f5cdf9092a36cdb1b1888cb9c
JaiSehgal007 <jaisehgal511@gmail.com>
2024-03-22 00:06:54 +0530

    Created Data Ingestion Pipeline

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from KidneyDiseaseClassifier import logger
+from KidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 # from src.KidneyDiseaseClassifier import logger
 # we need not write like this as KidneyDiseaseClassifier is installed as a local package
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nX================================X")
+except Exception as e:
+    logger.exception(e)
+    raise e
