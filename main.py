6145004c2b0ae264f60a1b941300e90b15a32d96
JaiSehgal007 <jaisehgal511@gmail.com>
2024-03-21 12:00:16 +0530

    Initialized Logger & Utility Files

---
+++ main.py
@@ -0,0 +1,4 @@
+from KidneyDiseaseClassifier import logger
+
+# from src.KidneyDiseaseClassifier import logger
+# we need not write like this as KidneyDiseaseClassifier is installed as a local package
