2d0b9b9046180ba604ef41783b7db6e7bd5c215c
JaiSehgal007 <jaisehgal511@gmail.com>
2024-03-22 11:50:24 +0530

    Created Base Model Prepration Pipeline

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from KidneyDiseaseClassifier import logger
 from KidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from KidneyDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 # from src.KidneyDiseaseClassifier import logger
 # we need not write like this as KidneyDiseaseClassifier is installed as a local package
@@ -10,6 +12,17 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nX================================X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nX===================X")
 except Exception as e:
     logger.exception(e)
     raise e
