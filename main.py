640f6d8bbea5d6ba91cb0ec82493cffb37817f1d
JaiSehgal007 <jaisehgal511@gmail.com>
2024-03-26 23:55:47 +0530

    Added Evaluation Pipeline using MLflow

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from KidneyDiseaseClassifier import logger
 from KidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from KidneyDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from KidneyDiseaseClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from KidneyDiseaseClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 # from src.KidneyDiseaseClassifier import logger
@@ -36,5 +37,17 @@ try:
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Evaluation Stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
         logger.exception(e)
         raise e
